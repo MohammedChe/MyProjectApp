ce0d903f03fc448dc37c261def4115dca752a517
Mohammed Che <mocherbatji@gmail.com>
2011-12-23 23:32:31 +0000

    none

---
+++ Register/insert.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$username="user_081a1e6e";
+$password='xlC4F!dR0%$A$&';
+$database="db_081a1e6e";
+
+$email=$_POST['email'];
+$passw=$_POST['passw'];
+
+
+mysql_connect(localhost,$username,$password);
+@mysql_select_db($database) or die( "Unable to select database");
+
+$query = "INSERT INTO User VALUES ('','$email','$passw')";
+mysql_query($query);
+
+mysql_close();
+?>
