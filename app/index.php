925cac7271b2cce9f8e340831c8d392ce11670c1
Mohammed Che <mocherbatji@gmail.com>
2011-12-23 20:09:42 +0000

    none

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@ $username="user_081a1e6e";
 $password="xlC4F!dR0%$A$&";
 $database="db_081a1e6e";
 
-mysql_connect("a.db.shared.orchestra.io",$username,$password);
+mysql_connect("a.db.shared.orchestra.io","user_081a1e6e","xlC4F!dR0%$A$&");
 
 @mysql_select_db($database) or die( "Unable to select database");
 
