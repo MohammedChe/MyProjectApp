97e73cbdf9654a5df58e70fbb811965b3568b801
Mohammed Che <mocherbatji@gmail.com>
2011-12-23 20:13:18 +0000

    noe

--- app/index.php
+++ app/index.php
@@ -4,9 +4,9 @@ $username="user_081a1e6e";
 $password="xlC4F!dR0%$A$&";
 $database="db_081a1e6e";
 
-mysql_connect("a.db.shared.orchestra.io","user_081a1e6e","xlC4F!dR0%$A$&");
+mysql_connect("http://a.db.shared.orchestra.io",$username,$password);
 
-@mysql_select_db("db_081a1e6e") or die( "Unable to select database");
+@mysql_select_db($database) or die( "Unable to select database");
 
 $query = "INSERT INTO Users VALUES ('','mo@hotmail.com','password')";
 
