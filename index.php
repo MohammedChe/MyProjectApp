8aadcf6afc759b14b47550f44cbd5fabe38dbc37
Mohammed Che <mocherbatji@gmail.com>
2011-12-23 22:19:18 +0000

    none

--- index.php
+++ index.php
@@ -21,3 +21,17 @@ mysql_query($query);
 mysql_close($con);
 
 ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>MyProjectApp</title>
+</head>
+
+<body>
+<p>
+TESTING
+</p>
+</body>
+</html>
