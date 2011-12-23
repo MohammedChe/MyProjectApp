3331b5e6f0c041b45674829886a63b834542e95e
Mohammed Che <mocherbatji@gmail.com>
2011-12-23 20:20:41 +0000

    none

--- app/index.php
+++ app/index.php
@@ -4,14 +4,20 @@ $username="user_081a1e6e";
 $password="xlC4F!dR0%$A$&";
 $database="db_081a1e6e";
 
-mysql_connect("http://a.db.shared.orchestra.io",$username,$password);
+$con = mysql_connect("a.db.shared.orchestra.io",$username,$password);
+if (!$con)
+{
+	die('Could not connect: ' . mysql_error());
+}
+
 
 @mysql_select_db($database) or die( "Unable to select database");
+
 
 $query = "INSERT INTO Users VALUES ('','mo@hotmail.com','password')";
 
 mysql_query($query);
 
-mysql_close();
+mysql_close($con);
 
 ?>
