e56991ccc15df2bd9728055f59c5118d92057f49
Mohammed Che <mocherbatji@gmail.com>
2011-12-23 20:57:11 +0000

    none

--- index.php
+++ index.php
@@ -1,52 +1,23 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="ltr" lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<?php
 
-<script type="text/javascript">
-function showRSS(str)
+$username="user_081a1e6e";
+$password="xlC4F!dR0%$A$&";
+$database="db_081a1e6e";
+
+$con = mysql_connect("a.db.shared.orchestra.io",$username,$password);
+if (!$con)
 {
-if (str.length==0)
-  { 
-  document.getElementById("rssOutput").innerHTML="";
-  return;
-  }
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-    document.getElementById("rssOutput").innerHTML=xmlhttp.responseText;
-    }
-  }
-xmlhttp.open("GET","getrss.php?q="+str,true);
-xmlhttp.send();
+	die('Could not connect: ' . mysql_error());
 }
-</script>
-<title>Orchesta Hosted Sample RSS Reader</title>
-</head>
-	
-<body>
-<h1>Orchestra-Hosted Sample RSS Reader</h1>
 
-<form>
-<select onchange="showRSS(this.value)">
-<option value="">Select an RSS-feed:</option>
-<option value="TeamworkPM">TeamworkPM</option>
-<option value="OnePageCRM">OnePageCRM</option>
-<option value="Allmoto">Allmoto</option>
-<option value="Intercom">Intercom</option>
-</select>
-</form>
-    
-<br />
-<h2>Check out their RSS feed here: </h2>
-<div id="rssOutput"></div>
-</body>
-</html>
+
+@mysql_select_db($database) or die( "Unable to select database");
+
+
+$query = "INSERT INTO Users VALUES ('','mo@hotmail.com','password')";
+
+mysql_query($query);
+
+mysql_close($con);
+
+?>
