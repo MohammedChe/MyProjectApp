e736384e44205da40b005f5022118ce211575f77
Mohammed Che <mocherbatji@gmail.com>
2011-12-24 15:24:29 +0000

    none

--- Register/register.php
+++ Register/register.php
@@ -12,66 +12,7 @@ $passcon=$_POST['passcon'];
 function validateEmail()
 {
    $isEmailValid = true;
-   $atIndex = strrpos($email, "@");
-   if (is_bool($atIndex) && !$atIndex)
-   {
-      $isEmailValid = false;
-   }
-   else
-   {
-      $domain = substr($email, $atIndex+1);
-      $local = substr($email, 0, $atIndex);
-      $localLen = strlen($local);
-      $domainLen = strlen($domain);
-      if ($localLen < 1 || $localLen > 64)
-      {
-         // local part length exceeded
-         $isEmailValid = false;
-      }
-      else if ($domainLen < 1 || $domainLen > 255)
-      {
-         // domain part length exceeded
-         $isEmailValid = false;
-      }
-      else if ($local[0] == '.' || $local[$localLen-1] == '.')
-      {
-         // local part starts or ends with '.'
-         $isEmailValid = false;
-      }
-      else if (preg_match('/\\.\\./', $local))
-      {
-         // local part has two consecutive dots
-         $isEmailValid = false;
-      }
-      else if (!preg_match('/^[A-Za-z0-9\-.]+$/', $domain))
-      {
-         // character not valid in domain part
-         $isEmailValid = false;
-      }
-      else if (preg_match('/\\.\\./', $domain))
-      {
-         // domain part has two consecutive dots
-         $isEmailValid = false;
-      }
-      else if
-(!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
-                 str_replace("\\\\","",$local)))
-      {
-         // character not valid in local part unless 
-         // local part is quoted
-         if (!preg_match('/^"(\\\\"|[^"])+"$/',
-             str_replace("\\\\","",$local)))
-         {
-            $isEmailValid = false;
-         }
-      }
-      if ($isEmailValid && !(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A")))
-      {
-         // domain not found in DNS
-         $isEmailValid = false;
-      }
-   }
-
+  
    return $isEmailValid;
 }
 
