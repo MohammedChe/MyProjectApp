24893ef682762f6ac2a08b34d5a302a7b3cb1ec2
Mohammed Che <mocherbatji@gmail.com>
2011-12-24 15:42:40 +0000

    none

--- Register/register.php
+++ Register/register.php
@@ -10,7 +10,15 @@ $passcon=$_POST['passcon'];
 
 function validateEmail()
 {
-   	return eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email);
+	if (!filter_input(INPUT_GET, "email", FILTER_VALIDATE_EMAIL))
+    {
+    	return false;
+    }
+	
+ 	else
+    {
+   		return true;
+    }
 }
 
 
