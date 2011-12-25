0c02108165d45e9fdaf5da1eb2cbb6ba101c903a
Mohammed Che <mocherbatji@gmail.com>
2011-12-25 20:01:46 +0000

    none

--- register.php
+++ register.php
@@ -41,73 +41,10 @@ if(isset($_POST['submit-form'])) {
 	    $success = false;
 	}
 	
-   $isValid = true;
-   $atIndex = strrpos($email, "@");
-   if (is_bool($atIndex) && !$atIndex)
-   {
-      $isValid = false;
-   }
-   else
-   {
-      $domain = substr($email, $atIndex+1);
-      $local = substr($email, 0, $atIndex);
-      $localLen = strlen($local);
-      $domainLen = strlen($domain);
-      if ($localLen < 1 || $localLen > 64)
-      {
-         // local part length exceeded
-         $isValid = false;
-      }
-      else if ($domainLen < 1 || $domainLen > 255)
-      {
-         // domain part length exceeded
-         $isValid = false;
-      }
-      else if ($local[0] == '.' || $local[$localLen-1] == '.')
-      {
-         // local part starts or ends with '.'
-         $isValid = false;
-      }
-      else if (preg_match('/\\.\\./', $local))
-      {
-         // local part has two consecutive dots
-         $isValid = false;
-      }
-      else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain))
-      {
-         // character not valid in domain part
-         $isValid = false;
-      }
-      else if (preg_match('/\\.\\./', $domain))
-      {
-         // domain part has two consecutive dots
-         $isValid = false;
-      }
-      else if
-(!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
-                 str_replace("\\\\","",$local)))
-      {
-         // character not valid in local part unless 
-         // local part is quoted
-         if (!preg_match('/^"(\\\\"|[^"])+"$/',
-             str_replace("\\\\","",$local)))
-         {
-            $isValid = false;
-         }
-      }
-      if ($isValid && !(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A")))
-      {
-         // domain not found in DNS
-         $isValid = false;
-      }
-   }
-   
-   if ($isValid){
-	   $success = true;
-   }
-   else{
-	   $success = false;
-   }
+	if(filter_var($email, FILTER_VALIDATE_EMAIL))
+	{
+		$success = false;
+	}
 
 	if($success)
 	{
@@ -134,21 +71,23 @@ if(isset($_POST['submit-form'])) {
 //If the form wasn't submitted, or didn't validate
 //then we show the registration form again
 ?>
-
-
 <html>
 <head>
-	<title>Registration</title>
+<title>Registration</title>
 </head>
 <body>
-	<?php echo ($error != "") ? $error : ""; ?>
-	<form action="register.php" method="post">
-    
-	E-Mail: <input type="text" value="<?php echo $email; ?>" name="email" /><br/>
-	Password: <input type="password" value="<?php echo $password; ?>" name="password" /><br/>
-	Password (confirm): <input type="password" value="<?php echo $password_confirm; ?>" name="password-confirm" /><br/>
-	<input type="submit" value="Register" name="submit-form" />
-	
-	</form>
+<?php echo ($error != "") ? $error : ""; ?>
+<form action="register.php" method="post">
+  E-Mail:
+  <input type="text" value="<?php echo $email; ?>" name="email" />
+  <br/>
+  Password:
+  <input type="password" value="<?php echo $password; ?>" name="password" />
+  <br/>
+  Password (confirm):
+  <input type="password" value="<?php echo $password_confirm; ?>" name="password-confirm" />
+  <br/>
+  <input type="submit" value="Register" name="submit-form" />
+</form>
 </body>
 </html>
