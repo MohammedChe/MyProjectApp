30242b02de9245f52dfb779afc7d13b7cdb72e10
Mohammed Che <mocherbatji@gmail.com>
2011-12-25 20:42:15 +0000

    none

--- home.php
+++ home.php
@@ -10,66 +10,32 @@ if(!isset($_SESSION['logged_in'])) {
 $user = unserialize($_SESSION['user']);
 
 //initialize php variables used in the form
-$email = "";
-$password = "";
-$password_confirm = "";
+$title = "";
 $error = "";
 
 //check to see that the form has been submitted
 if(isset($_POST['submit-form'])) { 
 
 	//retrieve the $_POST variables
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$password_confirm = $_POST['password-confirm'];
+	$title = $_POST['title'];
 
 	//initialize variables for form validation
 	$success = true;
 	$userTools = new UserTools();
 	
-	//validate that the form was filled out correctly
-	//check to see if user name already exists
-	if($userTools->checkEmailExists($email))
-	{
-	    $error .= "This email is already registered.<br/> \n\r";
-	    $success = false;
-	}
-	
-
-	if(strlen($password) < 6)
-	{
-	    $error .= "Password must be 6 characters or over.<br/> \n\r";
-	    $success = false;
-	}
-	//check to see if passwords match
-	if($password != $password_confirm) {
-	    $error .= "Passwords do not match.<br/> \n\r";
-	    $success = false;
-	}
-	
-	if ( filter_var($email, FILTER_VALIDATE_EMAIL)  == FALSE) 
-	{
-		$error .= "Email address not valid.<br/> \n\r";
-		$success = false;
-	}
-
 	if($success)
 	{
 	    //prep the data for saving in a new user object
-	    $data['email'] = $email;
-	    $data['password'] = md5($password); //encrypt the password for storage
+	    $data['title'] = $title;
 	
 	    //create the new user object
-	    $newUser = new User($data);
+	    $newCat = new Category($data);
 	
 	    //save the new user to the database
-	    $newUser->save(true);
-	
-	    //log them in
-	    $userTools->login($email, $password);
+	    $newCat->save(true);
 	
 	    //redirect them to a welcome page
-	    header("Location: welcome.php");
+	    header("Location: home.php");
 	    
 	}
 
