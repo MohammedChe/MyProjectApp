2c830e0d44d56ba62bc9b362a6e6e4304803421b
Mohammed Che <mocherbatji@gmail.com>
2011-12-27 23:56:17 +0000

    none

--- styled/index.php
+++ styled/index.php
@@ -209,7 +209,215 @@
 			</ul>
 		</div>
 			
+		<div id="side_nav" class="side_nav grid_3 push_down">
+			<ul class="clearfix">
+				<li><a class="round_left" href="#">
+					<img src="images/icons/grey/admin_user.png">
+					<span>Home</span></a>
+				</li> 
+				<li><a href="#">
+					<img src="images/icons/grey/settings_2.png">
+					<span>Slide</span>
+					<span class="icon">&nbsp;</span>
+					</a>
+					<ul>
+						<li><a href="#">North America</a></li>
+						<li><a href="#">Europe
+							<span class="icon">&nbsp;</span></a>	
+							<ul>
+								<li><a href="#">
+									<img src="images/icons/grey/sign_post.png">Eastern</a></li>
+								<li><a href="#">
+									<img src="images/icons/grey/strategy_2.png">Central</a></li>
+								<li><a href="#">
+									<img src="images/icons/grey/refresh_2.png">Western	
+									<span class="icon">&nbsp;</span></a>	
+									<ul>
+										<li><a href="#">
+											<img src="images/icons/grey/post_card.png">Germany</a></li>
+										<li><a href="#">
+											<img src="images/icons/grey/speech_bubble.png">Netherlands</a></li>
+										<li><a href="#">
+											<img src="images/icons/grey/tags_2.png">France
+											<span class="left icon">&nbsp;</span></a>								
+											<ul class="slide_left">
+												<li><a href="#">Paris</a></li>
+												<li><a href="#">Lyon</a></li>
+												<li><a href="#">Marseille</a></li>
+												<li><a href="#">Toulouse</a></li>
+											</ul>					
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li><a href="#">Asia</a></li>
+					</ul>
+				</li>				
+				<li><a href="#">
+					<img src="images/icons/grey/Battery.png">
+					<span>Slide Accordion</span>
+					<span class="icon">&nbsp;</span></a>														
+					<ul>
+						<li><a href="#">
+							<img src="images/icons/grey/zip_file.png">Fruits
+							<span class="icon">&nbsp;</span></a>
+							<div class="accordion">
+								<a href="#">Apples</a>
+								<a href="#">Oranges</a>
+								<a href="#">Bananas</a>
+							</div>	
+						</li>
+						<li class="link"><a href="http://en.wikipedia.org/wiki/Bread">
+							<img src="images/icons/grey/winner_podium.png">Breads</a>
+						</li>
+						<li><a href="#">
+							<img src="images/icons/grey/sport_shirt.png">Vegetables
+							<span class="icon">&nbsp;</span></a>
+							<div class="accordion">
+								<a href="#">Potatoes</a>
+								<a href="#">Spinach</a>
+								<a href="#">Celery</a>
+							</div>	
+						</li>
+						<li><a href="#">
+							<img src="images/icons/grey/trashcan_2.png">Meat
+							<span class="icon">&nbsp;</span></a>
+							<div class="accordion">
+								<a href="#">Beef</a>
+								<a href="#">Pork</a>
+								<a href="#">Venison</a>
+							</div>	
+						</li>
+					</ul>					
+				</li>	
+				<li><a href="#">
+					<img src="images/icons/grey/running_man.png">
+					<span>Accordion</span>
+					<span class="icon">&nbsp;</span></a>
+					<div class="accordion">
+						<a href="#">Apples</a>
+						<a href="#">Oranges</a>
+						<a href="#">Bananas</a>
+					</div>	
+				</li>	
+				<li><a href="#">
+					<img src="images/icons/grey/price_tag.png">
+					<span>Video Games</span>
+					<span class="icon">&nbsp;</span></a>
+					<div class="accordion">
+						<a href="#">XBox</a>
+						<a href="#">PS3</a>
+						<a href="#">Wii</a>
+					</div>	
+				</li>	
+				<li><a href="#">
+					<img src="images/icons/grey/chart_6.png">
+					<span>Mixed</span>
+					<span class="icon">&nbsp;</span></a>
+					<ul>
+						<li><a href="#">North America
+							<span class="icon">&nbsp;</span></a>
+							<div class="accordion">
+								<a href="#">Paris</a>
+								<a href="#">Lyon</a>
+								<a href="#">Toulouse</a>
+							</div>
+						</li>
+						<li><a href="#">Europe
+							<span class="icon">&nbsp;</span></a>	
+							<ul>
+								<li><a href="#">Eastern</a></li>
+								<li><a href="#">Central</a></li>
+								<li><a href="#">Western
+									<span class="icon">&nbsp;</span></a>		
+									<ul>
+										<li><a href="#">Germany</a></li>
+										<li><a href="#">Netherlands</a></li>
+										<li><a href="#">France
+											<span class="icon">&nbsp;</span></a>									
+											<div class="accordion">
+												<a href="#">Paris</a>
+												<a href="#">Lyon</a>
+												<a href="#">Marseille</a>
+											</div>					
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li><a href="#">Asia</a></li>
+					</ul>
+				</li>
+				<li><a href="#">
+					<img src="images/icons/grey/Book.png">
+					<span>Link</span></a>
+				</li>				
+				<li><a href="#">
+					<img src="images/icons/grey/magnifying_glass.png">
+					<span>Search</span>
+					<span class="icon">&nbsp;</span></a>
+					<div class="drop_box round_all">
+						<form style="width:210px">
+							<input class="round_all" value="Search...">
+							<button class="send_right">Go</button>
+						</form>
+					</div>
+				</li>				
+				<li><a href="#">
+					<img src="images/icons/grey/Key.png">
+					<span>Login</span>
+					<span class="icon">&nbsp;</span></a>
+					<div class="drop_box round_all">
+						<form style="width:160px">
+							<fieldset class="grid_8">
+								<label>Email</label><input class="round_all" value="name@example.com">
+							</fieldset>
+							<fieldset class="grid_8">
+								<label>Password</label><input class="round_all" type="password" value="password">
+							</fieldset>
+							<button class="send_right">Login</button>
+						</form>
+					</div>
+				</li>				
+			</ul>
+			<a href="#" class="minimize round_bottom"><span>minimize</span></a>
+		</div>
 		
+		<div id="main" class="grid_13 omega">
+			<div class="content round_all clearfix">
+				<div class=""><img src="images/sherpa_logo.png"></div>
+				
+				<div class="grid_16 alpha omega switcher">
+					<h3>Content Width</h3>
+					<div id="width_slider" style="width:100%;">&nbsp;</div>
+				</div>
+				<div id="colour_switcher" class="grid_16 alpha omega switcher">
+					<h3>Colour Themes</h3>
+					<a id="blue" href="#"><span>Blue</span></a>
+					<a id="red" href="#"><span>Red</span></a>
+					<a id="green" href="#"><span>Green</span></a>
+					<a id="cyan" href="#"><span>Cyan</span></a>
+					<a id="orange" href="#"><span>Orange</span></a>
+					<a id="pink" href="#"><span>Pink</span></a>
+					<a id="purple" href="#"><span>Purple</span></a>
+					<a id="navy" href="#"><span>Navy</span></a>
+					<a id="brown" href="#"><span>Brown</span></a>
+				</div>
+				<div id="bg_switcher" class="grid_16 alpha omega switcher">
+					<h3>Background Images</h3>
+					<a id="hatch" href="images/bg_hatch_grey_dark.jpg"><span>Hatch</span></a>
+					<a id="ash" href="images/bg_ash.jpg"><span>Ash</span></a>
+					<a id="brown_noise" href="images/bg_diag_wood.jpg"><span>Brown Noise</span></a>
+					<a id="dark_wood" href="images/bg_dark_wood.jpg"><span>Dark Wood</span></a>
+					<a id="holes" href="images/bg_holes.png"><span>Holes</span></a>
+					<a id="honeycomb" href="images/bg_honeycomb.png"><span>Honeycomb</span></a>
+					<a id="noise" href="images/bg_noise.png"><span>Noise</span></a>
+					<a id="punched" href="images/bg_punched.png"><span>Punched</span></a>
+					<a id="silver_noise" href="images/bg_silver_noise_grey.jpg"><span>Silver</span></a>
+					<a id="squares" href="images/bg_squares.png"><span>Squares</span></a>
+					<a id="wood" href="images/bg_wood.jpg"><span>Wood</span></a>
+				</div>
 			<p><strong>Sherpas</strong> were immeasurably valuable to early explorers of the Himalayan region, serving as guides and porters at the extreme altitudes of the peaks and passes in the region. Today, the term is used casually to refer to almost any guide or porter hired for mountaineering expeditions in the Himalayas. In Nepal, however, <strong>Sherpas</strong> insist on making the distinction between themselves and general porters, because <strong>Sherpas</strong> often serve in a more guide-like role and command higher pay and respect from the community.</p>
 			<p><strong>Sherpas</strong> are renowned in the international climbing and mountaineering community for their hardiness, expertise, and experience at high altitudes. It has been speculated that a portion of the <strong>Sherpas</strong>' climbing ability is the result of a genetic adaptation to living in high altitudes. Some of these adaptations include unique hemoglobin-binding enzymes, doubled nitric oxide production, hearts that can utilize glucose, and lungs with an increased efficiency in low oxygen conditions.</p>
 			<p><strong>Sherpas</strong> are short in stature to accelerate the speed of circulation around the body and also breathe more quickly than the average person to extract more oxygen from the thin air.</p>
