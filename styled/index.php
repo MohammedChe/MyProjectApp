ce57ef0fd9434830fcaa1c956aeabbc1a0c6fcc4
Mohammed Che <mocherbatji@gmail.com>
2011-12-28 01:37:17 +0000

    none

--- styled/index.php
+++ styled/index.php
@@ -74,7 +74,7 @@
 -->	
 					<img src="images/icons/grey/Book.png">
 
-					Categories
+					Bookmarks
 					<span class="icon">&nbsp;</span></a>
 					<ul>
 						<li><a href="#">North America</a></li>
@@ -110,48 +110,29 @@
 						<li><a href="#">Asia</a></li>
 					</ul>
 				</li>
-				<li><a href="#">
-					<img src="images/icons/grey/Battery.png">
-					Accordion
-					<span class="icon">&nbsp;</span></a> 														
-					<ul>
-						<li><a href="#">
-							<img src="images/icons/grey/zip_file.png">Fruits</a>
-							<div class="accordion">
-								<a href="#">Apples</a>
-								<a href="#">Oranges</a>
-								<a href="#">Bananas</a>
-							</div>	
-							<span class="icon">&nbsp;</span>
-						</li>
-						<li class="link"><a href="http://en.wikipedia.org/wiki/Bread">
-							<img src="images/icons/grey/winner_podium.png">Breads</a>
-						</li>
-						<li><a href="#">
-							<img src="images/icons/grey/sport_shirt.png">Vegetables</a>
-							<div class="accordion">
-								<a href="#">Potatoes</a>
-								<a href="#">Spinach</a>
-								<a href="#">Celery</a>
-							</div>	
-							<span class="icon">&nbsp;</span>
-						</li>
-						<li><a href="#">
-							<img src="images/icons/grey/trashcan_2.png">Meat</a>
-							<div class="accordion">
-								<a href="#">Beef</a>
-								<a href="#">Pork</a>
-								<a href="#">Venison</a>
-							</div>	
-							<span class="icon">&nbsp;</span>
-						</li>
-					</ul>					
-				</li>	
-							
-				<li><a href="#">
-					<img src="images/icons/grey/Book.png">
-					Link</a>
-				</li>				
+				<li class="has_mega_menu send_right"><a href="#">
+						<img src="images/icons/grey/Chrome.png">
+						Mega Menu
+						<span class="icon">&nbsp;</span></a>
+						<div class="mega_menu container_16"> 
+							<div class="grid_8"> 
+								<h2>Welcome to Sherpa Nav System</h2> 
+								<p><img class="float_left" src="images/sherpa_crest.jpg" width="132" height="132"><strong>Sherpas</strong> were immeasurably valuable to early explorers of the Himalayan region, serving as guides and porters at the extreme altitudes of the peaks and passes in the region. Today, the term is used casually to refer to almost any guide or porter hired for mountaineering expeditions in the Himalayas.</p>
+								<p>In Nepal, however, <strong>Sherpas</strong> insist on making the distinction between themselves and general porters, because <strong>Sherpas</strong> often serve in a more guide-like role and command higher pay and respect from the community.</p>
+							</div> 
+							<div class="grid_4"> 
+								<h4>1/4 Column</h4> 
+								<p><strong>Sherpas</strong> are short in stature to accelerate the speed of circulation around the body and also breathe more quickly than the average person to extract more oxygen from the thin air.</p>
+							</div> 
+							<div class="grid_4"> 
+								<h4>1/4 Column</h4> 
+								<p><strong>Sherpas</strong> are renowned in the international climbing and mountaineering community for their hardiness, expertise, and experience at high altitudes.</p>
+							</div> 
+							<div class="grid_8"> 
+								<p>It has been speculated that a portion of the <strong>Sherpas</strong>' climbing ability is the result of a genetic adaptation to living in high altitudes. Some of these adaptations include unique hemoglobin-binding enzymes, doubled nitric oxide production, hearts that can utilize glucose, and lungs with an increased efficiency in low oxygen conditions.</p>
+							</div> 
+						</div> 
+					</li>	
 				<li class="send_right"><a class="round_right" href="#">
 					<img src="images/icons/grey/magnifying_glass.png">
 					Search
