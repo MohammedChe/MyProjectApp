289e77af2c68c0802412cdaee1aa7fa513f732de
Mohammed Che <mocherbatji@gmail.com>
2011-12-28 01:17:08 +0000

    none

--- styled/index.php
+++ styled/index.php
@@ -36,8 +36,11 @@
 					Home</a>
 				</li> 
 				<li><a href="#">
-					<img src="images/icons/grey/settings_2.png">
-					Slide
+<!--					<img src="images/icons/grey/settings_2.png">
+-->	
+					<img src="images/icons/grey/Book.png">
+
+					Categories
 					<span class="icon">&nbsp;</span></a>
 					<ul>
 						<li><a href="#">North America</a></li>
@@ -109,47 +112,7 @@
 							<span class="icon">&nbsp;</span>
 						</li>
 					</ul>					
-				</li>
-				<li class="has_mega_menu"><a href="#">
-					<img src="images/icons/grey/Chrome.png">
-					Mega Menu
-					<span class="icon">&nbsp;</span></a>
-					<div class="mega_menu container_16"> 
-						<div class="grid_12"> 
-							<h2>Colour your experience</h2>
-                        </div>  
-                        
-                        
-                        <div id="colour_switcher" class="grid_16 alpha omega switcher">
-							<h3>Colour Themes</h3>
-							<a id="blue" href="#"><span>Blue</span></a>
-							<a id="red" href="#"><span>Red</span></a>
-							<a id="green" href="#"><span>Green</span></a>
-							<a id="cyan" href="#"><span>Cyan</span></a>
-							<a id="orange" href="#"><span>Orange</span></a>
-							<a id="pink" href="#"><span>Pink</span></a>
-							<a id="purple" href="#"><span>Purple</span></a>
-							<a id="navy" href="#"><span>Navy</span></a>
-							<a id="brown" href="#"><span>Brown</span></a>
-						</div>
-						<div id="bg_switcher" class="grid_16 alpha omega switcher">
-							<h3>Background Images</h3>
-							<a id="hatch" href="images/bg_hatch_grey_dark.jpg"><span>Hatch</span></a>
-							<a id="ash" href="images/bg_ash.jpg"><span>Ash</span></a>
-							<a id="brown_noise" href="images/bg_diag_wood.jpg"><span>Brown Noise</span></a>
-							<a id="dark_wood" href="images/bg_dark_wood.jpg"><span>Dark Wood</span></a>
-							<a id="holes" href="images/bg_holes.png"><span>Holes</span></a>
-							<a id="honeycomb" href="images/bg_honeycomb.png"><span>Honeycomb</span></a>
-							<a id="noise" href="images/bg_noise.png"><span>Noise</span></a>
-							<a id="punched" href="images/bg_punched.png"><span>Punched</span></a>
-							<a id="silver_noise" href="images/bg_silver_noise_grey.jpg"><span>Silver</span></a>
-							<a id="squares" href="images/bg_squares.png"><span>Squares</span></a>
-							<a id="wood" href="images/bg_wood.jpg"><span>Wood</span></a>
-						</div>
-                             
-						 
-					</div> 
-				</li>				
+				</li>	
 				<li><a href="#">
 					<img src="images/icons/grey/chart_6.png">
 					Mixed
