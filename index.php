19156a6d56d80f194077ca079c31272641ec4e42
Mohammed Che <mocherbatji@gmail.com>
2012-01-06 01:35:47 +0000

    none

--- index.php
+++ index.php
@@ -278,7 +278,7 @@ if(isset($_POST['submit-login'])) {
         <div id="main" class="box grid_4">
 			<div class="content round_all clearfix">
            
-                   <a href="http://strictlybeats.blogspot.com/2006/11/9th-wonder-instrumental-drop.html">
+                   <a target="_blank" href="http://strictlybeats.blogspot.com/2006/11/9th-wonder-instrumental-drop.html">
                    <img src="http://immediatenet.com/t/l?Size=1024x768&URL=http://strictlybeats.blogspot.com/2006/11/9th-wonder-instrumental-drop.html" /> </a> 
 
 			</div>
