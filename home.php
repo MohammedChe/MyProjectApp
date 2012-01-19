624ca0abba553328d737e5bb0d9b1fb5faf2f943
Mohammed Che <mocherbatji@gmail.com>
2012-01-19 21:41:21 +0000

    none

--- home.php
+++ home.php
@@ -468,22 +468,20 @@ else{
 <script type="text/javascript">
   $(function(){
 	  getMarks("recent", "Recent");
-  });
-  
-  
-  
-  
-  $(window).load(function() { buildWall(); });
-  
-  $(function() {
-    $(".imgHover").hover(
+	  
+	  $(".imgHover").hover(
         function() {
             $(this).children("img").fadeTo(200, 0.85).end().children(".hover").show();
         },
         function() {
             $(this).children("img").fadeTo(200, 1).end().children(".hover").hide();
         });
-});
+  });
+  
+  
+  
+  
+  $(window).load(function() { buildWall(); });
   
   
   $(window).resize(function(){
@@ -522,6 +520,7 @@ function removeMark(mark) {
 		$(this).empty().remove();
 		});
 }
+
 
 
 </script>
