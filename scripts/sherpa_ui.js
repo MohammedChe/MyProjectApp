56c63bd4c91995f561cffab74115e6a118e695b2
MohammedChe <mocherbatji@gmail.com>
2012-01-20 14:03:57 +0000

    Update scripts/sherpa_ui.js

--- scripts/sherpa_ui.js
+++ scripts/sherpa_ui.js
@@ -169,22 +169,31 @@ $(document).ready(function(){
 				});
 				
 		$("head").append( link );
-		
+		$.cookie("col_Link",link);
         
+		$.cookie("colour",colour);
+		$.cookie("self",$(this));
 		
-		if (colour.toLowerCase() == "default"){
-			 $("img").each(function(){
-        	      $(this).attr("src", $(this).attr("src").replace("white", "grey"));  
-       		 });
-		}
-		else{
-			$("img").each(function(){
-           	   $(this).attr("src", $(this).attr("src").replace("grey", "white"));  
-       		 });
+		changeImgColour(colour, $(this));
+		
 		}
 		
 		
 	});
+	
+	function changeImgColour(colour, self){
+		
+		if (colour.toLowerCase() == "default"){
+			 $("img").each(function(){
+        	      self.attr("src", self.attr("src").replace("white", "grey")); 
+        	      
+       		 });
+		}
+		else{
+			$("img").each(function(){
+           	   self.attr("src", self.attr("src").replace("grey", "white"));  
+       		 });
+	}
 	
 	$('#bg_switcher a').click(function(){
 		var link = $(this).attr('href');
@@ -213,6 +222,12 @@ $(document).ready(function(){
 			
 		
 	//Cookies for layout
+	if($.cookie('col_Link') != null) {
+		$("head").append( $.cookie("col_Link") );
+		changeImgColour($.cookie("colour"), $.cookie("self");
+	}
+		
+		
 	
 	if($.cookie('bg_Link') != null) {
 		$('body').css('background',$.cookie("bg_Link"));
