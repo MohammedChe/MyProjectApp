61a6d4fc613af2db8bbe943a3518bbce23776d33
Mohammed Cherbatji <mocherbatji@gmail.com>
2012-01-26 23:09:10 +0000

    none

--- mobile.php
+++ mobile.php
@@ -9,7 +9,34 @@
 
 
 
-echo "You are using a mobile device."
-
-
 ?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>MyProjectApp</title>
+
+    <link href="http://code.jquery.com/mobile/latest/jquery.mobile.min.css" rel="stylesheet" type="text/css" />
+    <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+    <script src="http://code.jquery.com/mobile/latest/jquery.mobile.min.js"></script>
+
+</head>
+<body>
+
+<div data-role="page">
+
+    <div data-role="header">
+        <h1>My Title</h1>
+    </div><!-- /header -->
+
+    <div data-role="content">
+        <p>Hello world</p>
+    </div><!-- /content -->
+
+</div><!-- /page -->
+
+</body>
+</html>
