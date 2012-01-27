3751d2bf3e414d10ed224d50fe3144a027767d36
Mohammed Cherbatji <mocherbatji@gmail.com>
2012-01-27 00:37:07 +0000

    none

--- mobile.php
+++ mobile.php
@@ -270,7 +270,7 @@ else{
 
     $( document ).bind( "mobileinit", function() {
         // Make your jQuery Mobile framework configuration changes here!
-
+        $.support.cors = true;
         $.mobile.allowCrossDomainPages = true;
     });
 </script>
