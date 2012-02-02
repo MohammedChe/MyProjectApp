540087c76ee36f073a41207d0ce4edd354c2bf43
Mohammed Cherbatji <mocherbatji@gmail.com>
2012-02-02 20:08:33 +0000

    none

--- index.php
+++ index.php
@@ -349,15 +349,15 @@ else{
                     {
                         $scheme = parse_url($value["url"], PHP_URL_SCHEME);
                         $host = parse_url($value["url"], PHP_URL_HOST);
-                        $theURL2 = $scheme . "://" . $host;
+                        $theURL = $scheme . "://" . $host;
 
                         $catTitle = $userTools->getCatTitle($value["category"], $user->id);
                         ?>
 
 
                         <li><a href="<?php echo htmlentities($value["url"]);?>">
-                            <img src="http://immediatenet.com/t/fs?Size=800x600&URL=<?php echo $theURL2;?>" />
-                            <h3 class="addLeftMargin"><?php echo $theURL2;?></h3>
+                            <img src="http://immediatenet.com/t/fs?Size=800x600&URL=<?php echo $theURL;?>" />
+                            <h3 class="addLeftMargin"><?php echo $host;?></h3>
                             <p class="addLeftMargin"><?php echo $catTitle["title"]; ?></p>
                         </a><a href="#" onClick="removeMark(<?php echo htmlentities($value["id"])?>,'<?php echo $redCat?>','<?php echo $selectedCat ?>');"  data-transition="slideup">Delete
                         </a></li>
@@ -370,14 +370,14 @@ else{
                     if(isset($recentMarks["url"])){
                         $scheme = parse_url($recentMarks["url"], PHP_URL_SCHEME);
                         $host = parse_url($recentMarks["url"], PHP_URL_HOST);
-                        $theURL2 = $scheme . "://" . $host;
+                        $theURL = $scheme . "://" . $host;
 
                         $catTitle = $userTools->getCatTitle($recentMarks["category"], $user->id);
                         ?>
 
                         <li><a href="<?php echo htmlentities($recentMarks["url"]);?>">
-                            <img src="http://immediatenet.com/t/fs?Size=800x600&URL=<?php echo $theURL2;?>" />
-                            <h3 class="addLeftMargin"><?php echo $theURL2;?></h3>
+                            <img src="http://immediatenet.com/t/fs?Size=800x600&URL=<?php echo $theURL;?>" />
+                            <h3 class="addLeftMargin"><?php echo $host;?></h3>
                             <p class="addLeftMargin"><?php echo $catTitle["title"]; ?></p>
                         </a><a href="#" onClick="removeMark(<?php echo htmlentities($recentMarks["id"])?>,'<?php echo $redCat?>','<?php echo $selectedCat ?>');"  data-transition="slideup">Delete
                         </a></li>
