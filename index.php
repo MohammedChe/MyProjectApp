8e19410a68e773d26760ec885ee5d35a0b17bcda
Mohammed Cherbatji <mocherbatji@gmail.com>
2012-02-02 19:49:51 +0000

    none

--- index.php
+++ index.php
@@ -345,13 +345,15 @@ else{
                         $scheme = parse_url($value["url"], PHP_URL_SCHEME);
                         $host = parse_url($value["url"], PHP_URL_HOST);
                         $theURL2 = $scheme . "://" . $host;
+
+                        $catTitle = $userTools->getCatTitle($value["category"], $user->id);
                         ?>
 
 
                         <li><a href="<?php echo htmlentities($value["url"]);?>">
                             <img src="http://immediatenet.com/t/fs?Size=800x600&URL=<?php echo $theURL2;?>" />
                             <h3 class="addLeftMargin"><?php echo $theURL2;?></h3>
-                            <p class="addLeftMargin"><?php echo $userTools->getCatTitle($value["category"], $user->id); ?></p>
+                            <p class="addLeftMargin"><?php echo $catTitle[0]; ?></p>
                         </a><a href="#" onClick="removeMark(<?php echo htmlentities($value["id"])?>,'<?php echo $redCat?>','<?php echo $selectedCat ?>');"  data-transition="slideup">Delete
                         </a></li>
 
@@ -367,12 +369,14 @@ else{
                         $scheme = parse_url($recentMarks["url"], PHP_URL_SCHEME);
                         $host = parse_url($recentMarks["url"], PHP_URL_HOST);
                         $theURL2 = $scheme . "://" . $host;
+
+                        $catTitle = $userTools->getCatTitle($recentMarks["category"], $user->id);
                         ?>
 
                         <li><a href="<?php echo htmlentities($recentMarks["url"]);?>">
                             <img src="http://immediatenet.com/t/fs?Size=800x600&URL=<?php echo $theURL2;?>" />
                             <h3 class="addLeftMargin"><?php echo $theURL2;?></h3>
-                            <p class="addLeftMargin"><?php echo $userTools->getCatTitle($value["category"], $user->id); ?></p>
+                            <p class="addLeftMargin"><?php echo $catTitle[0]; ?></p>
                         </a><a href="#" onClick="removeMark(<?php echo htmlentities($recentMarks["id"])?>,'<?php echo $redCat?>','<?php echo $selectedCat ?>');"  data-transition="slideup">Delete
                         </a></li>
 
