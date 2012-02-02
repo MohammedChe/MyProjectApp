fe2b7fb14fd9a9e2ec7528d2e6d37d65d1088784
Mohammed Cherbatji <mocherbatji@gmail.com>
2012-02-02 21:36:54 +0000

    none

--- index.php
+++ index.php
@@ -526,7 +526,7 @@ else{
     function getMarks(catId) {
         $.post('getMarks.php', {c:catId},
             function (output) {
-                $('#container').html(output).fadeIn(800);
+                html(output).fadeIn(800);
             });
     }
 
