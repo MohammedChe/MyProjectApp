7366531d208f7a814f53902d551148d8306cf099
Mohammed Cherbatji <mocherbatji@gmail.com>
2012-02-05 19:54:45 +0000

    none

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 
 require_once 'includes/global.inc.php';
 
+header("Pragma: no-cache");
+header("cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
 //if(isset($_GET['r'])) {
 //    header("Location: index.php");
 //    exit;
