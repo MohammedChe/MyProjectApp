8b40df75358210aef15a2be4883faf154f3d6b97
Mohammed Cherbatji <mocherbatji@gmail.com>
2012-02-10 22:25:06 +0000

    none

--- home.php
+++ home.php
@@ -317,7 +317,7 @@ else {
 
                         <li data-role="list-divider"><?php echo htmlentities($value["title"])?><span class="ui-li-count">
                         <?php echo $catCount["total"];?>
-                        </span><a href="removeCat.php?c=<?php echo $value["id"];?>" data-ajax="false"><span class="ui-li-count addRightMargin">x</a></span></li>
+                        </span><span class="ui-icon ui-icon-plus ui-icon-shadow" id="add"><a href="removeCat.php?c=<?php echo $value["id"];?>" data-ajax="false">Delete</a></span></li>
 
                         <?php
 
@@ -387,7 +387,7 @@ else {
 
                         <li data-role="list-divider"><?php echo htmlentities($cat["title"])?><span class="ui-li-count">
                         <?php echo $catCount["total"];?>
-                    </span><a href="removeCat.php?c=<?php echo $cat["id"];?>" data-ajax="false"><span class="ui-li-count addRightMargin">x/span></a><</li>
+                    </span><span class="ui-icon ui-icon-plus ui-icon-shadow" id="add"><a href="removeCat.php?c=<?php echo $cat["id"];?>" data-ajax="false">Delete</a></span></li>
 
                         <?php
 
