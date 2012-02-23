b8c28fdec5229fb02fc53f6ca71ace8b6585d6d7
Mohammed Cherbatji <mocherbatji@gmail.com>
2012-02-23 06:32:02 +0000

    added add button for native app

---
+++ app/addBookmark.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: MohammedChe
+ * Date: 26/01/2012
+ * Time: 16:41
+ * To change this template use File | Settings | File Templates.
+ */
+
+require_once 'includes/global.inc.php';
+
+if (!isset($_SESSION['logged_in'])) {
+    header('Location: index.php');
+}
+
+else {
+    $user = unserialize($_SESSION['user']);
+
+    
+    $url = $_GET['url'];
+    $cat = $_GET['select-choice-a'];
+    $note = $_GET['note'];
+
+        //initialize variables for form validation
+    $userTools = new UserTools();
+    $checkedURL = $userTools->checkURL($url);
+
+    if (isset($checkedURL) && $checkedURL != false) {
+        //prep the data for saving in a new user object
+        $data['category'] = $cat;
+        $data['owner'] = $user;
+        $data['url'] = $checkedURL;
+        $data['note'] = $note;
+
+        $newBookmark = new Bookmark($data);
+
+        if($newBookmark->save(true))
+        {
+        	echo "Bookmark Added";
+        }
+        else
+        {
+        	echo "Sorry your bookmark was not added, Please try again later";
+        }
+
+    }
+    else
+    {
+        echo "URL doesn't exist";
+    }
+}
+
+?>
---
+++ app/addCategory.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: MohammedChe
+ * Date: 26/01/2012
+ * Time: 16:41
+ * To change this template use File | Settings | File Templates.
+ */
+
+require_once 'includes/global.inc.php';
+
+if (!isset($_SESSION['logged_in'])) {
+    header('Location: index.php');
+}
+
+else {
+    $user = unserialize($_SESSION['user']);
+
+    //retrieve the $_POST variables
+    $title = $_GET['t'];
+
+    $userTools = new UserTools();
+
+    //prep the data for saving in a new user object
+    $data['title'] = $title;
+    $data['owner'] = $user;
+    
+    //create the new user object
+    $newCat = new Category($data);
+
+    //save the new user to the database
+    if($newCat->save(true))
+    {
+    	echo "Category Added";
+    }
+    else
+    {
+    	echo "Sorry your Category was not added, Please try again later";
+    }
+
+}
+
+?>
